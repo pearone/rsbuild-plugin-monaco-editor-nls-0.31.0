8e9e05d296d1b8ff85b863401e20b9540754d95f
xupengyi <xupengyi@58.com>
2024-11-12 14:33:33 +0800

    fix: 0.33.0

--- playground/rsbuild.config.ts
+++ playground/rsbuild.config.ts
@@ -3,7 +3,7 @@ import { pluginReact } from '@rsbuild/plugin-react';
 import {
   Languages,
   pluginMonacoEditorNls,
-} from 'rsbuild-plugin-monaco-editor-nls';
+} from 'rsbuild-plugin-monaco-editor-nls-0.31.0';
 
 export default defineConfig({
   plugins: [
